45b12baecc8d29f79556badcb564f690501594ca
vander <vanderdeng@gmail.com>
2022-10-20 19:51:48 +1000

    fix: add jwt-decode library

--- apps/galaxy/src/utils/tokenService.ts
+++ apps/galaxy/src/utils/tokenService.ts
@@ -1,3 +1,9 @@
+import jwtDecode from "jwt-decode";
+
+type jwtToken = {
+  exp: number;
+};
+
 export const setToken = (token: string) => {
   localStorage.setItem("accessToken", token);
 };
@@ -10,23 +16,9 @@ export const getToken = () => {
 
 export const checkTokenExpiration = () => {
   const token = getToken();
+  const parseToken: jwtToken = jwtDecode(token as string);
   const utcTimestamp = new Date();
-  return utcTimestamp > parseJwtToken(token as string);
-};
-
-const parseJwtToken = (token: string) => {
-  const base64Url = token.split(".")[1];
-  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  const jsonPayload = decodeURIComponent(
-    window
-      .atob(base64)
-      .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
   const exp = new Date(0);
-  exp.setUTCSeconds(JSON.parse(jsonPayload).exp);
-  return exp;
+  exp.setUTCSeconds(parseToken?.exp as number);
+  return utcTimestamp > exp;
 };
