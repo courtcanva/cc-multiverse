be4ce11e654d83705733ba20b87bb989c7b642a1
vander <vanderdeng@gmail.com>
2022-10-22 19:20:32 +1000

    fix: code review

--- apps/galaxy/src/services/utils/axios.ts
+++ apps/galaxy/src/services/utils/axios.ts
@@ -3,14 +3,14 @@ import axios from "axios";
 import { environment } from "../constants/environment";
 import { getToken } from "@src/utils/tokenService";
 
-const token = getToken();
+const token = getToken() || "";
 const REQUEST_TIMEOUT = 2000;
 
 const customAxios = axios.create({
   baseURL: environment.API_BASE_URL,
   timeout: REQUEST_TIMEOUT,
   headers: {
-    Authorization: token as string,
+    Authorization: token,
   },
 });
 
