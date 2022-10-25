13e564ec70c5092c809d8b88e336ddf1f831eaba
Kerry Yuan <jiayuu0531@outlook.com>
2022-10-25 14:54:54 +1100

    fix: code update

--- apps/galaxy/src/pages/service-area-selection.tsx
+++ apps/galaxy/src/pages/service-area-selection.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from "next";
-import HeaderLayout from "../layouts/HeaderLayout";
+import Head from "../utils/Head";
 import ServiceAreaSelection from "@src/modules/ServiceAreaSelectionForm/index";
 
 const ServiceAreaSelectionPage: NextPage = () => {
   return (
-    <HeaderLayout title="Select Service Area">
+    <Head title="Select Service Area">
       <ServiceAreaSelection />
-    </HeaderLayout>
+    </Head>
   );
 };
 
