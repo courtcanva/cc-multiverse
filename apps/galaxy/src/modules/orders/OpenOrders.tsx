3eb09cba909975180b4b771db3f611ee044bb3d6
Winnie Chen <winniechen@Winnies-MacBook-Air.local>
2022-10-27 22:15:27 +1100

    add axios post

--- apps/galaxy/src/modules/orders/OpenOrders.tsx
+++ apps/galaxy/src/modules/orders/OpenOrders.tsx
@@ -23,14 +23,67 @@ import {
   PopoverAnchor,
   Button,
 } from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "../../services/utils/axios";
 
 const OpenOrders = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isLoading, setIsLoading] = useState(false);
+  const [details, setDetails] = useState("");
+  const onOpenPopUp = async () => {
+    onOpen;
+    setIsLoading(true);
+    try {
+      const response = await axios.post("/franchisee/1/accept_orders", {
+        body: { orders: [{ id: 1 }, { id: 2 }, { id: 3 }] },
+      });
+      if (response.status === 200) {
+        setDetails(JSON.stringify(response));
+        console.log("hello ------------------------ " + response);
+      }
+    } catch (error) {
+      console.log("error is here -------------------" + error);
+      // const err = error as AxiosError;
+      // if (err.response?.status === 400) {
+      //   toast({
+      //     title: "Sign in failed",
+      //     description: "Username and password is not authenticated",
+      //     status: "error",
+      //     duration: 6000,
+      //     position: "top",
+      //     isClosable: true,
+      //   });
+      // } else {
+      //   toast({
+      //     title: err.message,
+      //     status: "error",
+      //     duration: 6000,
+      //     position: "top",
+      //     isClosable: true,
+      //   });
+      // }
+    }
+    setIsLoading(false);
+
+    // fetch("http://127.0.0.1:8080/api/v1/franchisee/1/accept_orders", {
+    //   body: {
+    //     orders: [{ id: 1 }, { id: 2 }, { id: 3 }],
+    //   },
+    //   content-type: "application/json; charset=utf8",
+    //   method: "POST",
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setDetails(data);
+    //     console.log("hello --------------------- " + data);
+    //   });
+  };
+
   return (
     <Flex>
       <Text>inside open orders</Text>
 
-      <Button onClick={onOpen}>Open Modal</Button>
+      <Button onClick={onOpenPopUp}>Open Modal</Button>
 
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
@@ -39,6 +92,8 @@ const OpenOrders = () => {
           <ModalCloseButton />
           <ModalBody>
             <Text>Inside modalBody</Text>
+            <Text>is it loading? {isLoading}</Text>
+            <Text>details here {details}</Text>
             <Box boxSize="sm">
               <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
             </Box>
