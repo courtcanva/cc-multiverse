c157551dc34582ee242d31240433232dfdee7a6d
Winnie Chen <winniechen@Winnies-MacBook-Air.local>
2022-11-01 20:41:42 +1100

    solve merge conflict

--- apps/galaxy/src/modules/orders/index.tsx
+++ apps/galaxy/src/modules/orders/index.tsx
@@ -3,12 +3,17 @@ import { Flex, Image, Text } from "@cc/ui-chakra";
 
 const Orders = () => {
   return (
-    <>
+    <Flex>
       <Text fontSize="23" fontWeight="bold">
         Open Orders
       </Text>
       <OpenOrders />
-    </>
+    </Flex>
+    <Flex>
+      <Flex>
+      </Flex>
+      <Flex></Flex>
+    </Flex>
   );
 };
 
