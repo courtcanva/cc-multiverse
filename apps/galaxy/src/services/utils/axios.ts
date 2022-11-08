e921173a5a5c467c591efa3e77bdfeb2f2b51aa3
vander <vanderdeng@gmail.com>
2022-11-08 12:22:24 +1000

    fix: add axios interceptor

--- apps/galaxy/src/services/utils/axios.ts
+++ apps/galaxy/src/services/utils/axios.ts
@@ -3,15 +3,36 @@ import axios from "axios";
 import { environment } from "../constants/environment";
 import { getToken } from "@src/utils/tokenService";
 
-const token = getToken() || "";
 const REQUEST_TIMEOUT = 2000;
+
+type config = {
+  url: string;
+};
+
+const urlWhiteList = ["/staff/signin", "/franchisee/signup"];
 
 const customAxios = axios.create({
   baseURL: environment.API_BASE_URL,
   timeout: REQUEST_TIMEOUT,
-  headers: {
-    Authorization: token,
-  },
 });
 
+customAxios.interceptors.request.use(
+  function (config) {
+    const { url } = <config>config;
+    const token = getToken() || "";
+    const customConfig = {
+      ...config,
+      headers: {
+        ...config.headers,
+        Authorization: urlWhiteList.includes(url) ? "" : token,
+      },
+    };
+    return customConfig;
+  },
+
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
 export default customAxios;
