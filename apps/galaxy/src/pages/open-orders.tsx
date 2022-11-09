f159d1f9019e291e32e6bc020dd7ba292afd7ff2
Gordon <sgz886@gmail.com>
2022-11-09 22:52:26 +1100

    chore: open-orders.tsx

---
+++ apps/galaxy/src/pages/open-orders.tsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from "react";
+
+class OpenOrders extends PureComponent {
+  render() {
+    return <div>order</div>;
+  }
+}
+
+// OpenOrders.propTypes = {};
+
+export default OpenOrders;
