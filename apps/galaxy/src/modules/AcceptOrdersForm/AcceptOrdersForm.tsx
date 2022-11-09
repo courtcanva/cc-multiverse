708b25651cca90857c9b48a73d848a9f47c45cc0
Iris99999 <753891368@qq.com>
2022-11-09 21:21:10 +0800

    reduce unused line

--- apps/galaxy/src/modules/AcceptOrdersForm/AcceptOrdersForm.tsx
+++ apps/galaxy/src/modules/AcceptOrdersForm/AcceptOrdersForm.tsx
@@ -1,5 +1,3 @@
-// import userGetOrders from "../../services/orders/assignedOrders";
-// import { useForm, SubmitHandler } from "react-hook-form";
 import {
   Checkbox,
   CheckboxGroup,
@@ -8,71 +6,9 @@ import {
   FormControl,
   Container,
 } from "@chakra-ui/react";
-// import { useState, useEffect, SetStateAction } from "react";
-// 上面旧
-
-// import { Button, Container, FormControl, FormLabel } from "@cc/ui-chakra";
 import { useEffect, useState } from "react";
-// import useOrders, { FormData } from "../../services/orders/useOrders";
 import { useForm } from "react-hook-form";
-// import { ErrorMessage } from "@hookform/error-message";
 import useGetOrders, { FormData } from "../../services/orders/useOrders";
-
-// import { Select as ReactSelect, createFilter } from "chakra-react-select";
-// 1.首先获取所有上一步筛选出的order的list
-// 2.function进项set更改状态（1）click默认状态是unchecked点击一次改变一下
-// 3.点击submit以后将状态为 checked的对应的id放入一个list
-// 4.将list传入后端
-// 5.test
-// interface OrderData {
-//   id: number;
-// }
-
-// interface OrderGroup {
-//   label: string;
-//   value: number;
-// }
-
-// interface FormData {
-//   orders: OrderData[];
-// }
-
-// const defaultValues: FormData = { orders: [] };
-
-// const ServiceAreaSelection = () => {
-// const [, setOptions] = useState<OrderGroup[]>([]);
-// // eslint-disable-next-line react-hooks/exhaustive-deps
-// const ordersArr: OrderGroup[] = [];
-// const { isLoading, handleAcceptOrderSubmit, getOpenOrders } = useGetOrders();
-// const { handleSubmit } = useForm<FormData>({
-//   defaultValues,
-// });
-
-// const submit: SubmitHandler<FormData> = (data) => {
-//   handleAcceptOrderSubmit(data);
-// };
-
-// useEffect(() => {
-//   getOpenOrders().then((result) => {
-//     // result.map((order: OrderData) => {
-//     //   const label = `${order.id}`;
-//     //   return ordersArr.push({ value: order.id, label: label });
-//     // });
-//     // setOptions(ordersArr);
-//   });
-// }, [getOpenOrders, ordersArr]);
-
-// return (
-//   <Container as="form" onSubmit={handleSubmit(submit)}>
-//     <HStack marginTop="48px">
-//       <Button type="submit" isLoading={isLoading} variant="primary">
-//         Submit
-//       </Button>
-//     </HStack>
-//   </Container>
-// );
-
-// 新
 interface OrderData {
   id: number;
 }
@@ -81,26 +17,21 @@ const AcceptOrders = () => {
   const { isLoading, getOpenOrders, handleAcceptOrderSubmit } = useGetOrders();
   const [lists, setList] = useState([]);
   const [checked, setChecked] = useState([]);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { control, handleSubmit } = useForm<FormData>({
+  const { handleSubmit } = useForm<FormData>({
     defaultValues,
   });
-  const change = (e) => {
+  const change = (e: (string | number)[]) => {
     setChecked(e);
   };
   const sub = () => {
-    // eslint-disable-next-line no-console
-    // console.log(data);
     const orders: Array<OrderData> = checked.map((val) => ({ id: val }));
-    handleAcceptOrderSubmit(orders).then(() => {
-      // console.log(res);
-    });
+    handleAcceptOrderSubmit(orders).then();
   };
   useEffect(() => {
     getOpenOrders().then((res) => {
-      // console.log(res);
       setList(res);
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <Container name="suburbs" as="form" onSubmit={handleSubmit(sub)}>
--- apps/galaxy/src/services/orders/useOrders.ts
+++ apps/galaxy/src/services/orders/useOrders.ts
@@ -2,21 +2,10 @@ import axios from "../utils/axios";
 import { useToast } from "@cc/ui-chakra";
 import { useState } from "react";
 import { AxiosError } from "axios";
-// import { useRouter } from "next/router";
-// import { getToken, getFranchiseeId } from "@src/utils/tokenService";
-
-// export interface Order {
-//   label: string;
-//   value: number;
-// }
 export interface Order {
   label: string;
   value: number;
 }
-
-// interface OrderGroup {
-//   orders: Order[];
-// }
 export interface FormData {
   orders: OrderData[];
 }
@@ -27,23 +16,13 @@ interface OrderData {
 
 export default function useGetOrders() {
   const [isLoading, setIsLoading] = useState(false);
-  // const [options, setOptions] = useState<Order[]>([]);
   const toast = useToast();
-  // const router = useRouter();
 
   const getOpenOrders = async () => {
     try {
       const response = await axios.get("/api/franchisee/1/pending_orders");
-      // const response = await axios.get("/franchisee/1/pending_orders");
       const result = response.data;
       return result;
-      // const result: OrderData[] = response.data.orders;
-      // const ordersArr = result.map(({ id }) => {
-      //   const label = `${id}`;
-      //   return { value: id, label };
-      // });
-      // setOptions(ordersArr);
-      // return options;
     } catch (error) {
       const err = error as AxiosError;
       toast({
@@ -62,12 +41,8 @@ export default function useGetOrders() {
 
     try {
       await axios.post("/api/franchisee/1/accept_orders", {
-        // const response = await axios.post("/franchisee/1/accept_orders", {
         orders: data,
       });
-      // if (response.status === 200) {
-      //   router.push("/");
-      // }
     } catch (error) {
       const err = error as AxiosError;
       if (err.response?.status === 400) {
