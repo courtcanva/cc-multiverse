9e5d85e45d938dfa1829167df8cc888bed9394df
Iris99999 <753891368@qq.com>
2022-11-11 21:02:51 +0800

    format

--- apps/galaxy/src/services/orders/useOrders.ts
+++ apps/galaxy/src/services/orders/useOrders.ts
@@ -51,7 +51,6 @@ export default function useGetOrders() {
 
   useEffect(() => {
     getOpenOrders();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleAcceptOrderSubmit = async (data: OrderIdList[]) => {
