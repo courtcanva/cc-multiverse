59fb2caa0b7596d42b366f01c3e27cb331b370ca
Winnie Chen <winniechen@Winnies-MacBook-Air.local>
2022-11-14 23:25:31 +1100

    modify order page layout

--- apps/galaxy/src/modules/orders/index.tsx
+++ apps/galaxy/src/modules/orders/index.tsx
@@ -4,22 +4,17 @@ import { Grid, GridItem, Image, Text } from "@cc/ui-chakra";
 const Orders = () => {
   return (
     <Grid
-      templateAreas={`"header header"
-                  "nav main"`}
-      gridTemplateRows={"112px 808px"}
+      templateAreas={`"nav main"`}
       gridTemplateColumns={"263px 945px"}
       h="920px"
       gap="1"
       // color="blackAlpha.700"
       fontWeight="bold"
     >
-      <GridItem pl="5px" bg="rgba(54, 73, 93, 0.16)" area={"header"}>
-        Header
-      </GridItem>
       <GridItem pl="5px" bg="rgba(54, 73, 93, 0.16)" area={"nav"}>
         Nav
       </GridItem>
-      <GridItem pl="5px" bg="green.300" area={"main"}>
+      <GridItem pl="5px" area={"main"}>
         <OpenOrders />
       </GridItem>
     </Grid>
