024348e1288b3d90ab8bc9967eac9add1ecd8939
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2022-11-19 13:34:28 +1100

    refactor: add sign out btn

--- apps/galaxy/src/pages/index.tsx
+++ apps/galaxy/src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { Stack, Button } from "@cc/ui-chakra";
+import { useRouter } from "next/router";
 import NoFoundIcon from "@src/assets/icons/404.svg";
 
 export default function Galaxy() {
+  const router = useRouter();
   const onToGalaxy = () => {
     console.log("Welcome to galaxy");
+  };
+  const signOut = () => {
+    localStorage.removeItem("accessToken");
+    router.push("/sign-in");
   };
 
   return (
@@ -11,6 +17,7 @@ export default function Galaxy() {
       <h1>Galaxy</h1>
       <NoFoundIcon />
       <Button onClick={onToGalaxy}>To Galaxy... and Beyong!!</Button>
+      <Button onClick={signOut}>Sign Out</Button>
     </Stack>
   );
 }
