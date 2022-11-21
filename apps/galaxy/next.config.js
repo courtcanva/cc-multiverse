7084afeaf3f3585729ca9335954a005a0161deb1
vander <vanderdeng@gmail.com>
2022-11-21 21:27:15 +1000

    bugfix: prepare update

--- apps/galaxy/next.config.js
+++ apps/galaxy/next.config.js
@@ -23,4 +23,21 @@ module.exports = withTM({
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/about",
+        headers: [
+          {
+            key: "x-custom-header",
+            value: "my custom header value",
+          },
+          {
+            key: "x-another-custom-header",
+            value: "my other custom header value",
+          },
+        ],
+      },
+    ];
+  },
 });
