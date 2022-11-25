dc4a32da96a4502259463f99c40904bb7ef8c5dd
vander <vanderdeng@gmail.com>
2022-11-25 21:33:18 +1000

    bugfix: type update

--- packages/ui-chakra/src/general/Table/DataTable.tsx
+++ packages/ui-chakra/src/general/Table/DataTable.tsx
@@ -31,32 +31,52 @@ export function DataTable<Data extends object>({ data, columns }: DataTableProps
   return (
     <Table>
       <Thead bg="gray.50">
-        {table.getHeaderGroups().map((headerGroup) => (
-          <Tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => {
-              const meta: any = header.column.columnDef.meta;
-              return (
-                <Th key={header.id} isNumeric={meta?.isNumeric}>
-                  <Center>{flexRender(header.column.columnDef.header, header.getContext())}</Center>
-                </Th>
-              );
-            })}
-          </Tr>
-        ))}
+        {table
+          .getHeaderGroups()
+          .map((headerGroup: { id: React.Key | null | undefined; headers: any[] }) => (
+            <Tr key={headerGroup.id}>
+              {headerGroup.headers.map(
+                (header: {
+                  column: { columnDef: { meta: any; header: any } };
+                  id: React.Key | null | undefined;
+                  getContext: () => any;
+                }) => {
+                  const meta: any = header.column.columnDef.meta;
+                  return (
+                    <Th key={header.id} isNumeric={meta?.isNumeric}>
+                      <Center>
+                        {flexRender(header.column.columnDef.header, header.getContext())}
+                      </Center>
+                    </Th>
+                  );
+                }
+              )}
+            </Tr>
+          ))}
       </Thead>
       <Tbody>
-        {table.getRowModel().rows.map((row) => (
-          <Tr key={row.id}>
-            {row.getVisibleCells().map((cell) => {
-              const meta: any = cell.column.columnDef.meta;
-              return (
-                <Td key={cell.id} isNumeric={meta?.isNumeric}>
-                  <Center>{flexRender(cell.column.columnDef.cell, cell.getContext())}</Center>
-                </Td>
-              );
-            })}
-          </Tr>
-        ))}
+        {table
+          .getRowModel()
+          .rows.map((row: { id: React.Key | null | undefined; getVisibleCells: () => any[] }) => (
+            <Tr key={row.id}>
+              {row
+                .getVisibleCells()
+                .map(
+                  (cell: {
+                    column: { columnDef: { meta: any; cell: any } };
+                    id: React.Key | null | undefined;
+                    getContext: () => any;
+                  }) => {
+                    const meta: any = cell.column.columnDef.meta;
+                    return (
+                      <Td key={cell.id} isNumeric={meta?.isNumeric}>
+                        <Center>{flexRender(cell.column.columnDef.cell, cell.getContext())}</Center>
+                      </Td>
+                    );
+                  }
+                )}
+            </Tr>
+          ))}
       </Tbody>
     </Table>
   );
