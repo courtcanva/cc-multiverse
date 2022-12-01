1f8581fe36f453705c898c57b638f7428edb15f9
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2022-12-02 00:49:11 +1100

    refactor: sidebar hyperlink color

--- apps/galaxy/src/modules/Orders/OpenOrders/index.tsx
+++ apps/galaxy/src/modules/Orders/OpenOrders/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, HStack, VStack, Sidebar } from "@cc/ui-chakra";
+import { Flex, HStack, VStack, Image, Text, Sidebar } from "@cc/ui-chakra";
 import OpenOrdersList from "@src/modules/Orders/OpenOrders/OpenOrdersList";
 import React from "react";
 import Logo from "@src/components/Logo";
@@ -9,7 +9,12 @@ const OpenOrders = () => {
       <Sidebar />
       <VStack alignContent="center" alignItems="center" h="100%" width="100%">
         <HStack alignItems="center" marginY="20px">
-          <Logo />
+          <Image
+            boxSize="64px"
+            alt="dashboard-logo-192x192"
+            src="/assets/dashboard-logo-192x192.png"
+          />
+          <Text fontSize="1.5rem">CourtCanva</Text>
         </HStack>
         <OpenOrdersList />
       </VStack>
