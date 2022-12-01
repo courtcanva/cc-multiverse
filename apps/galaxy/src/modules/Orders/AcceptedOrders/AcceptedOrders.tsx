a50b3beb93b0843417a18009774364410055d683
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2022-12-02 02:48:44 +1100

    refactor: adding accepted order service

---
+++ apps/galaxy/src/modules/Orders/AcceptedOrders/AcceptedOrders.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { Button, Checkbox, DataTable, Stack, HStack, VStack } from "@cc/ui-chakra";
+import useGetOrders, { Order } from "@src/services/orders/useAcceptedOrders";
+import dayjs from "dayjs";
+import { createColumnHelper } from "@tanstack/react-table";
+import Details from "../../Orders/Details";
+
+const AcceptedOrdersList = () => {
+  const { lists } = useGetOrders();
+  const columnHelper = createColumnHelper<Order>();
+  const [checkedItems, setCheckedItems] = React.useState([false]);
+  const allChecked = checkedItems.every(Boolean);
+  const isIndeterminate = checkedItems.some(Boolean) && !allChecked;
+
+  React.useEffect(() => {
+    (checkedItems.length = lists.length), checkedItems.fill(false, 0, lists.length);
+  }, [lists]);
+
+  const columns = [
+    columnHelper.accessor("id", {
+      cell: (info) => (
+        <Checkbox
+          onChange={(e) => {
+            const list = checkedItems;
+            checkedItems[info.row.index] = e.target.checked;
+            setCheckedItems([...list]);
+          }}
+          isChecked={checkedItems[info.row.index]}
+        />
+      ),
+      header: () => (
+        <Checkbox
+          isChecked={allChecked}
+          isIndeterminate={isIndeterminate}
+          onChange={(e) => {
+            setCheckedItems([...checkedItems.fill(e.target.checked, 0, lists.length)]);
+          }}
+        />
+      ),
+    }),
+    columnHelper.accessor("createdTime", {
+      cell: (info) => dayjs(info.getValue()).format("DD/MM/YYYY"),
+      header: "date",
+    }),
+    columnHelper.accessor("designInformation", {
+      cell: (info) => info.getValue().constructionAddress?.city,
+      header: "suburb",
+    }),
+    columnHelper.accessor("postcode", {
+      cell: (info) => info.getValue(),
+      header: "postcode",
+      meta: {
+        isNumeric: true,
+      },
+    }),
+    columnHelper.accessor("totalAmount", {
+      cell: (info) => "$" + info.getValue(),
+      header: "total amount",
+      meta: {
+        isNumeric: true,
+      },
+    }),
+    columnHelper.accessor("designInformation", {
+      cell: (info) => <Details {...info} />,
+      header: "details",
+      id: "details",
+    }),
+  ];
+  return (
+    <VStack>
+      <Stack
+        border="1px"
+        borderBottom="0px"
+        borderColor="gray.200"
+        borderRadius="lg"
+        overflow="hidden"
+      >
+        <DataTable columns={columns} data={lists} />
+      </Stack>
+    </VStack>
+  );
+};
+
+export default AcceptedOrdersList;
